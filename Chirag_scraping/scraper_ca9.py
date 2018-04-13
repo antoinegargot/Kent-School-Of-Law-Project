a5c194315dbebd49654c8ce98733c9b25ce42454
leochirag <crao3@hawk.iit.edu>
2018-04-13 16:46:01 -0500

    Scraped new features

--- Chirag_scraping/scraper_ca9.py
+++ Chirag_scraping/scraper_ca9.py
@@ -73,6 +73,120 @@ def get_citation_details(soup):
 
     return opinions_cited_id, opinions_cited_links
 
+def get_date_submitted(soup):
+    date_submitted = ''
+    center_tags = soup.find_all("center")
+    for dates in center_tags:
+        try:
+            if "Submitted" in dates.get_text():
+                #print dates
+                date_submitted = dates.get_text().encode("utf-8")
+        except:
+            print "Date submitted not found"
+
+    return date_submitted
+
+def get_date_filed(soup):
+    date_filed = ''
+    center_tags = soup.find_all("center")
+    for filed in center_tags:
+        try:
+            if "Filed" in filed.get_text():
+                #print filed
+                date_filed = filed.get_text().encode("utf-8")
+        except:
+            print "Date Filed not found"
+
+    return date_filed
+
+def get_judge_name(soup):
+    judge_name = ''
+    judge_tag = soup.find_all("p")
+    for name in judge_tag:
+        try:
+            if "District Judge:" in name.get_text():
+                #print name
+                judge_name = name.get_text().encode("utf-8")
+            elif "Circuit Judge:" in name.get_text():
+                #print name
+                judge_name = name.get_text().encode("utf-8")
+        except:
+            print "Judge names not found"
+
+    return judge_name
+
+def get_case_cites(soup):
+    case_cite = ''
+    cite_tag = soup.find_all(class_="case_cite")
+    for cite in cite_tag:
+        try:
+            #print cite.get_text()
+            case_cite = cite.get_text().encode("utf-8")
+        except:
+            print "case cites not found"
+
+    return case_cite
+
+def get_parties(soup):
+    parties = ''
+    parties_tag = soup.find_all(class_="parties")
+    for p in parties_tag:
+        try:
+            #print p.get_text()
+            parties = p.get_text().encode("utf-8")
+        except:
+            print "Parties not found"
+
+    return parties
+
+def get_docket(soup):       #Same as Case-no.
+    docket = ''
+    docket_tag = soup.find_all(class_="docket")
+    for d in docket_tag:
+        try:
+            #print d.get_text()
+            docket = d.get_text().encode("utf-8")
+        except:
+            print "Case no. not found"
+
+    return docket
+def get_court(soup):
+    court = ''
+    court_tag = soup.find_all(class_="court")
+    for courts in court_tag:
+        try:
+            #print courts.get_text()
+            court = courts.get_text().encode("utf-8")
+        except:
+            print "Courts data not found"
+
+    return court
+def get_dates(soup):
+    dates = ''
+    date_new = soup.find_all(class_="date")
+    for date in date_new:
+        try:
+            #print date.get_text()
+            dates = date.get_text().encode("utf-8")
+        except:
+            print "Citation not found"
+
+    return dates
+
+def get_indent(soup):
+    indent = ''
+    indent_tag = soup.find_all(class_="indent")
+    for i in indent_tag:
+        try:
+            if "for appell" in i.get_text():
+                #print i
+                indent = i.get_text().encode('utf-8')
+        except:
+            print "Data not found"
+
+    return indent
+
+
 def main():
     dataset_dict = {}
 
@@ -80,6 +194,15 @@ def main():
     case_name = []
     opinion_id = []
     opinion_link = []
+    date_submitted = []
+    date_filed = []
+    judge_name = []
+    case_cite = []
+    parties = []
+    docket = []
+    court = []
+    indent = []
+    dates = []
 
     file_paths = get_file_paths()
     for files in file_paths:
@@ -89,6 +212,24 @@ def main():
         case_id_str, case_name_str = get_case_details(soup)
         case_id.append(case_id_str)
         case_name.append(case_name_str)
+        date_submitted_str = get_date_submitted(soup)
+        date_submitted.append(date_submitted_str)
+        date_filed_str = get_date_filed(soup)
+        date_filed.append(date_filed_str)
+        judge_name_str = get_judge_name(soup)
+        judge_name.append(judge_name_str)
+        case_cite_str = get_case_cites(soup)
+        case_cite.append(case_cite_str)
+        parties_str = get_parties(soup)
+        parties.append(parties_str)
+        docket_str = get_docket(soup)
+        docket.append(docket_str)
+        court_str = get_court(soup)
+        court.append(court_str)
+        indent_str = get_indent(soup)
+        indent.append(indent_str)
+        dates_str = get_dates(soup)
+        dates.append(dates_str)
 
         opinions_cited_id, opinions_cited_links = get_citation_details(soup)
 
@@ -100,7 +241,15 @@ def main():
     dataset_dict["Case_Name"] = case_name
     dataset_dict["Opinions_cited"] = opinion_id
     dataset_dict["Opinions_links"] = opinion_link
-
+    dataset_dict["Date Submitted"] = date_submitted
+    dataset_dict["Date Filed"] = date_filed
+    dataset_dict["Judges Names"] = judge_name
+    dataset_dict["Case Cites"] = case_cite
+    dataset_dict["Parties"] = parties
+    dataset_dict["Docket"] = docket
+    dataset_dict["Court"] = court
+    dataset_dict["Indent Names"] = indent
+    dataset_dict["Date"] = dates
     # Convert to DataFrame
     df = pd.DataFrame(dataset_dict)
 
